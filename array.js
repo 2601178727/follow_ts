f1416d2712a4ed7cb9f2c5277e1c8a14a590c8f4
best <1404076435@qq.com>
2019-02-12 13:15:15 +0800

    feat

--- array.js
+++ array.js
@@ -1,2 +1,4 @@
-var arr = [1, 3, 4];
+// let arr: number[] = [1, 3, 4]; // 数组
+var arr = [1, 3, 4]; // 数组泛型
 arr.push(1);
+var ar = [1, '1', { a: 1 }];
--- array.ts
+++ array.ts
@@ -1,3 +1,5 @@
 // let arr: number[] = [1, 3, 4]; // 数组
 let arr: Array<number> = [1, 3, 4]; // 数组泛型
 arr.push(1);
+
+let ar: any[] = [1, '1', {a: 1}];
